7793ec87600a64d4d5d929a14f1994322b200d8c
Gabriela Kadzielawa <gabrielakadzielawa@Gabrielas-MacBook.local>
2022-06-04 15:05:31 -1000

    first commit miewww

---
+++ backend/app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+// HIDE YOUR MONGO CONNECTION VARIABLES
+
+require('dotenv').config();
+
+// MONGO DB CONNECTION
+
+mongoose.connect(process.env.DB_URI, {
+  dbName: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  pass: process.env.DB_PASS,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(res => console.log("MongoDB connected!"))
+.catch(err => console.log(err));
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Lets go to the moon miewww</h1>
+    </div>
+  );
+}
+
+export default App;
