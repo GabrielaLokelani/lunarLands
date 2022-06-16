feadcb3a5c99fd3b7e8f4006f838d8291327d5aa
“GabrielaLokelani” <gabrielakadzielawa@gmail.com>
2022-06-15 17:04:02 -1000

    connect frontend to backend, FE

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,14 +1,23 @@
-import data from '../data'
+
 import { Link } from 'react-router-dom'
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 const HomeScreen = () => {
+    const [estates, setEstates] = useState([]);
+    useEffect(()=> {
+        const fetchData = async () => {
+            const result = await axios.get('http://localhost:2600/api/estates');
+            setEstates(result.data);
+        };
+        fetchData();
+    }, []);
     return ( 
         <div>
-            <h1>look at allll these lunar estates</h1>
-
+        <h1>look at allll these lunar estates</h1>
         <h1>Featured Estates</h1> 
         <div className='estates'>
-        {data.estates.map((estate) => (
+        {estates.map((estate) => (
         <div className='estate' key={estate.slug}>
           <Link to={`/estate/${estate.slug}`}>
             <img src={estate.image} alt={estate.title} /> 
@@ -17,12 +26,18 @@ const HomeScreen = () => {
             <Link to={`/estate/${estate.slug}`}>
               <p> {estate.title} </p>
             </Link>
-              <p> 
-                <strong>${estate.price}</strong>
-              </p>
-              <button>Purchase</button>
+            <p> 
+            {estate.location}
+            </p>
+            <p> 
+            {estate.description}
+            </p>
+            <p> 
+            <strong>${estate.price}</strong>
+            </p>
+            <button>Purchase</button>
             </div>
-          </div>
+            </div>
         ))}
         </div>
         </div> 
