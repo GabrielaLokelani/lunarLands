eb4bac86ed708d53babb3425374d66df4cf23abd
“GabrielaLokelani” <gabrielakadzielawa@gmail.com>
2022-06-18 10:37:12 -1000

    put app in dark mode

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 
 import './App.css';
 import { BrowserRouter, Link, Route, Routes} from 'react-router-dom'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 import EstateScreen from './screens/EstateSceen';
 import HomeScreen from './screens/HomeScreen';
 import RegisterScreen from './screens/RegisterScreen';
@@ -8,27 +10,38 @@ import LoginScreen from './screens/LoginScreen';
 
 
 
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
 function App() {
+
   return (
     <div className="App">
+       <ThemeProvider theme={darkTheme}>
+       <CssBaseline />
       <BrowserRouter>
     <div>
       <header>
         <Link to='/'>Lunar Lands </Link>
 
-        <Link to='/users/register'> Register </Link>
-        <Link to='/users/login'> Login </Link>
+        <Link to='/user/register'> Register </Link>
+        <Link to='/user/login'> Login </Link>
       </header>
       <main>
         <Routes>
-          <Route path='/users/login' element={<LoginScreen/>} />
-          <Route path='/users/register' element={<RegisterScreen/>} />
+          <Route path='/user/login' element={<LoginScreen/>} />
+          <Route path='/user/register' element={<RegisterScreen/>} />
           <Route path='/estate/:slug' element={<EstateScreen/>} />
           <Route path='/' element={<HomeScreen/>} />
         </Routes>
       </main>
     </div>
     </BrowserRouter>
+    </ThemeProvider>
     </div>
   );
 }
